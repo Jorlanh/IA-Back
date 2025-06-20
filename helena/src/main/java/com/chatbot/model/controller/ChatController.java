0748dc7b4e1a5bc78488af327f42cecec469c9dc
Jorlanh <jorlan25.js@gmail.com>
2025-06-20 15:35:10 -0300

    ofc

--- helena/src/main/java/com/chatbot/model/controller/ChatController.java
+++ helena/src/main/java/com/chatbot/model/controller/ChatController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://ia-front-i75b.onrender.com") // Permite acesso do frontend React
+@CrossOrigin(origins = "https://helena-ia.onrender.com") // Permite acesso do frontend React
 public class ChatController {
 
     private final HelenaChatService chatService;
